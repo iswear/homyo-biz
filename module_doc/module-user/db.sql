f3c1f5da07b98daa8ecf91c482c66780458d6160
iswear <471291492@qq.com>
2018-03-31 00:49:20 +0800

    game

--- module_doc/module-user/db.sql
+++ module_doc/module-user/db.sql
@@ -15,20 +15,36 @@ create table `user` (
     primary key (`id`)
 ) engine = InnoDB default charset = utf8 comment = '用户表';
 
+-- 用户session记录表
+create table `session` (
+    `id` bigint unsigned not null default '0' comment '用户ID',
+    `create_time` timestamp not null default current_timestamp comment '创建时间',
+    `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
+    `session_id` varchar(64) not null default '' comment '会话SESSION',
+    `platform` int(11) unsigned not null default '' comment '平台：1.IOS客户端；2.ANDROID客户端；3.移动WEB；4.PCWEB',
+    `login_time` timestamp not null default current_timestamp comment '登录时间',
+    `version` varchar(16) not null default '' comment '客户端版本',
+    `ip` varchar(16) not null default '' comment '用户IP',
+    `user_id` bigint unsigned not null default '' comment '用户ID',
+    `deleted` tinyint(1) not null default '' comment '是否已经删除；0.未删除；1.已删除',
+    primary key(`id`)
+) engine = InnoDB default charset = utf8 comment = '用户session记录表';
+
 -- 用户社交信息表
-create table `user_society` (
+create table `society` (
     `id` bigint unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
     `platform` int(11) unsigned not null default '0' comment '1.微信；2.微博；3.博客；4.QQ；5.个人网站；6.github',
     `content` varchar(255) not null default '' comment '内容',
     `des` varchar(255) not null default '' comment '描述',
+    `user_id` bigint unsigned null default '' comment '用户ID',
     `deleted` tinyint(1) not null default '0' comment '是否已经删除：0.未删除；1.已删除', 
     primary key (`id`)
 ) engine = InnoDB default charset = utf8 comment = '用户社交网络信息表';
 
 -- 角色表
-create table `user_role` (
+create table `role` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update,
@@ -38,8 +54,8 @@ create table `user_role` (
     primary key(`id`)
 ) engine = InnoDB default charset = utf8 comment = '角色';
 
--- 权限表
-create table `user_api` (
+-- 接口表
+create table `api` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
@@ -63,7 +79,7 @@ create table `user_role_relation` (
 ) engine = InnoDB default charset = utf8 comment = '用户角色关系';
 
 -- 角色权限关系表
-create table `user_role_api_relation` (
+create table `role_api_relation` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
@@ -74,7 +90,7 @@ create table `user_role_api_relation` (
 ) engine = InnoDB default charset = utf8 comment = '橘色权限关系';
 
 -- 用户微信绑定表
-create table `user_wx` (
+create table `binder_wx` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
@@ -82,7 +98,7 @@ create table `user_wx` (
 ) engine = InnoDB default charset = utf8 comment = '用户微信绑定';
 
 -- 用户微博绑定表
-create table `user_wb` (
+create table `binder_wb` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
@@ -90,7 +106,7 @@ create table `user_wb` (
 ) engine = InnoDB default charset = utf8 comment = '用户微博绑定';
 
 -- 用户qq绑定表
-create table `user_qq` (
+create table `binder_qq` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
@@ -98,7 +114,7 @@ create table `user_qq` (
 ) engine = InnoDB default charset = utf8 comment = '用户QQ绑定';
 
 -- 用户github绑定表
-create table `user_github` (
+create table `binder_github` (
     `id` int(11) unsigned not null default '0' comment 'ID',
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
